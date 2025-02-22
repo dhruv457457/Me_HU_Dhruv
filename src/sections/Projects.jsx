5fb094202984c1a09f53ea02dc141ef3921dbe8d
Dhruv Pancholi <dpancholi.pp123@gmail.com>
2025-02-22 23:38:34 +0530

    Update Projects.jsx

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,26 +1,31 @@
-import React, { useState } from 'react';
 import styles from './Projects.module.css';
-import ProjectsCard from '../common/ProjectCard.jsx';
+import Ayurtherb from "../assets/Ayurherb-proj.png";
+import CrowdFund from "../assets/crowdfund-static.png";
+import Cryptify from "../assets/cryptify-static.jpeg";
+import ProjectCard from '../common/ProjectCard.jsx';
 
 function Projects() {
   return (
     <section id='projects' className={styles.container}>
       <h1 className='sectionTitle'>Projects</h1>
       <div className={styles.ProjectsContainer}>
-        <ProjectsCard 
+        <ProjectCard 
           link={'https://herb-sphere.vercel.app/'}
-          h3={'Ayurherb'}
-          p={'The Virtual Herbal Garden'}
+          title={'Ayurherb'}
+          description={'The Virtual Herbal Garden'}
+          staticSrc={Ayurtherb}
         />
-        <ProjectsCard 
+        <ProjectCard 
           link={'https://cryptify-defi.vercel.app/'}
-          h3={'Cryptify DeFi'}
-          p={'A secure SWC & smart payment platform'}
+          title={'Cryptify DeFi'}
+          description={'A secure SWC & payment platform'}
+          staticSrc={Cryptify}
         />
-        <ProjectsCard 
+        <ProjectCard 
           link={'https://fund-raising-on-flow-chain.vercel.app/'}
-          h3={'Crowdfunding DApp'}
-          p={'A decentralized crowdfunding platform'}
+          title={'Crowdfunding DApp'}
+          description={'A crowdfunding Dapp'}
+          staticSrc={CrowdFund}
         />
       </div>
     </section>
